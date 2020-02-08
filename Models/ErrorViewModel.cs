1ebbf8d340db62f90d45862d54d4e19a8cbc3fda
alakialakiani <alakialakiani@yahoo.com>
2020-02-08 20:57:11 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Almas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
